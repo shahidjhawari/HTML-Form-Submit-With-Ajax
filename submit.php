ad8b18a078c91a194acb993d87bf3b6d357c69df
Mahar Shahid Iqbal Jhawari <shahidiqbaljhawari@gmail.com>
2023-12-01 10:51:41 +0500

    insert data into database

--- submit.php
+++ submit.php
@@ -1,3 +1,11 @@
 <?php 
+include('db.php');
+$name=$_POST['name'];
+$username=$_POST['username'];
+$mobile=$_POST['mobile'];
+$password=$_POST['password'];
 
+$sql="insert into data(name,username,mobile,pwd) values('$name','$username','$mobile','$password')";
+$stmt=$con->prepare($sql);
+$stmt->execute();
 ?>
